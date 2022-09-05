1212a14646bcca71f61dc63d89544444ce869d85
ahmetknk <aemresafak@gmail.com>
2022-09-05 19:07:06 +0300

    Initial Commit

---
+++ Anofilm Filters/Anofilm_FiltersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anofilm_FiltersApp.swift
+//  Anofilm Filters
+//
+//  Created by ahmetknk on 5.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Anofilm_FiltersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
