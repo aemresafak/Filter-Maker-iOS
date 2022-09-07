37133b54520d3ca5846af7f224b7215d419690fa
ahmetknk <aemresafak@gmail.com>
2022-09-07 18:33:52 +0300

    made color codable

---
+++ Anofilm Filters/data/errors/ColorError.swift
@@ -0,0 +1,13 @@
+//
+//  ColorError.swift
+//  Anofilm Filters
+//
+//  Created by ahmetknk on 7.09.2022.
+//
+
+import Foundation
+import SwiftUI
+
+enum ColorError : Error {
+    case componentError
+}
--- Anofilm Filters/extensions/ColorExtensions.swift
+++ Anofilm Filters/extensions/ColorExtensions.swift
@@ -25,3 +25,36 @@ extension Color {
         Color(.sRGB, red: Double(vector.x), green: Double(vector.y), blue: Double(vector.z), opacity: 1)
     }
 }
+
+extension Color : Codable {
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Color.CodingKeys.self)
+        let red = try container.decode(Double.self, forKey: .red)
+        let green = try container.decode(Double.self, forKey: .green)
+        let blue = try container.decode(Double.self, forKey: .blue)
+        let alpha = try container.decode(Double.self, forKey: .alpha)
+        self = Color(.sRGB, red: red, green: green, blue: blue, opacity: alpha)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        let color = UIColor(self)
+        guard let components = color.cgColor.components else {
+            throw ColorError.componentError
+        }
+        let red = Double(components[0])
+        let green = Double(components[1])
+        let blue = Double(components[2])
+        let alpha = Double(components[3])
+        var container = encoder.container(keyedBy: Color.CodingKeys.self)
+        try container.encode(red, forKey: .red)
+        try container.encode(green, forKey: .green)
+        try container.encode(blue, forKey: .blue)
+        try container.encode(alpha, forKey: .alpha)
+    }
+    
+    
+    private enum CodingKeys: String, CodingKey  {
+        case red, green, blue, alpha
+    }
+}
