b615719a0bc6a12866454ac21a44f1b7b6299161
ahmetknk <aemresafak@gmail.com>
2022-09-13 11:03:26 +0300

    changed sepia tone matrix

--- Anofilm Filters/data/customfilters/MTISepiaToneFilter.swift
+++ Anofilm Filters/data/customfilters/MTISepiaToneFilter.swift
@@ -10,21 +10,21 @@ import MetalPetal
 
 class MTISepiaToneFilter: MTICustomColorMatrixFilter {
     static let sepiaToneFilter = simd_float4x4(rows: [
-        SIMD4(0.3588, 0.7044, 0.1368, 0),
-        SIMD4(0.2990, 0.5870, 0.1140, 0),
-        SIMD4(0.2392, 0.4696, 0.0912, 0),
-        SIMD4(0,0,0,1)
+        SIMD4(0.43922, 0, 0, 0),
+        SIMD4(0, 0.25882, 0, 0),
+        SIMD4(0, 0, 0.07843, 0),
+        SIMD4(0, 0, 0, 1)
     ])
-    
-   
+
+
     init() {
         super.init(matrix: MTISepiaToneFilter.sepiaToneFilter)
     }
-    
-   
-    
+
+
+
     required init(from decoder: Decoder) throws {
         try super.init(from: decoder)
     }
-    
+
 }
