8620ce91d2c658d35bfef744331fc0e82b7ea65c
ahmetknk <aemresafak@gmail.com>
2022-09-16 11:51:25 +0300

    created blue levels adjustment view

---
+++ Anofilm Filters/data/customfilters/MTIBlueLevelsAdjustment.swift
@@ -0,0 +1,55 @@
+//
+//  MTIBlueLevelsAdjustment.swift
+//  Anofilm Filters
+//
+//  Created by ahmetknk on 16.09.2022.
+//
+
+import Foundation
+import MetalPetal
+
+class MTIBlueLevelsAdjustment: MTILevelsAdjustmentFilter {
+
+
+    var minimumLevel: Float = 0 {
+        willSet {
+            minimumColor = simd_float3(0, 0, newValue)
+        }
+    }
+    var middleLevel: Float = 1 {
+        willSet {
+            middleColor = simd_float3(1, 1, newValue)
+        }
+    }
+    var maximumLevel: Float = 1 {
+        willSet {
+            maximumColor = simd_float3(1, 1, newValue)
+        }
+    }
+
+    var minimumOutputLevel: Float = 0 {
+        willSet {
+            minOutputColor = simd_float3(0, 0, newValue)
+        }
+    }
+
+    var maximumOutputLevel: Float = 1 {
+        willSet {
+            maxOutputColor = simd_float3(1, 1, newValue)
+        }
+    }
+
+    override init() {
+        super.init()
+        minimumColor = simd_float3(0, 0, 0)
+        middleColor = simd_float3(1, 1, 1)
+        maximumColor = simd_float3(1, 1, 1)
+        minOutputColor = simd_float3(0, 0, 0)
+        maxOutputColor = simd_float3(1, 1, 1)
+    }
+
+    required init(from decoder: Decoder) throws {
+        try super.init(from: decoder)
+    }
+}
+
